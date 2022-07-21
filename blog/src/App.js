3126182ca050d6a1ab8b731388648be6e9c7c4a8
Jongchan <tolotol@naver.com>
2022-07-21 18:14:28 +0900

    react JSX 문법

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 
 function App() {
   return (
+
+  // 실은 JSX - js파일에서 쓰는 html 대용품
+  // JSX안에서는 className
+  
     <div className="App">
       <div className="black-nav">
           <div style>개발 Blog</div>
