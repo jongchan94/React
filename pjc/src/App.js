9955dbe91ab0fee56162a06ec261700b070779b4
Jongchan <tolotol@naver.com>
2023-03-28 01:20:13 +0900

    // JSX 문법 3가지 // className // 데이터바인딩(변수 넣는곳) {} // style 넣을 땐 style = { { 스타일명 : '값'} }  주의사항 font-size는 fontSize

--- pjc/src/App.js
+++ pjc/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+// JSX 문법 3가지
+// className
+// 데이터바인딩(변수 넣는곳) {}
+// style 넣을 땐 style = { { 스타일명 : '값'} }  주의사항 font-size는 fontSize
+
+
 function App() {
+
+  let post = '강남 우동 맛집';
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={ {color : 'red'}}>블로그임</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
