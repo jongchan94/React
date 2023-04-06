e1d91d3153ee54109d88f899bb28c7ccfa6a863f
Jongchan <tolotol@naver.com>
2023-04-06 13:21:14 +0900

    //state 쓰면 자동으로 재 렌더링함  //변수대신 쓰는 이유

--- pjc/src/App.js
+++ pjc/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState} from 'react';
 // JSX 문법 3가지
 // className
 // 데이터바인딩(변수 넣는곳) {}
@@ -10,14 +10,25 @@ import './App.css';
 function App() {
 
   let post = '강남 우동 맛집';
+ //state 쓰면 자동으로 재 렌더링함
+  let [a,b] = useState('남자 코트 추천')
 
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color : 'red'}}>블로그임</h4>
+        <div>개발 blog</div>
       </div>
-      <h4>{post}</h4>
+
+      <div className="list">
+        <h4>{a}</h4>
+        <p>4월 6일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>글제목</h4>
+        <p>4월 6일 발행</p>
+      </div>
     </div>
   );
 }
